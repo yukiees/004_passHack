1bba23a10b91f759d19754bfbd62f97fbca74cee
yukiees <yukiii8989@gmail.com>
2020-11-14 02:46:11 +0900

    Pass UI

--- 004_passHack/ViewController.swift
+++ 004_passHack/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel1: UILabel!
+    @IBOutlet weak var resultLabel2: UILabel!
+    @IBOutlet weak var resultLabel3: UILabel!
+    @IBOutlet weak var resultLabel4: UILabel!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func start(_ sender: Any) {
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
 }
 
