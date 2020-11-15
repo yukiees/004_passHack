f6cffe04f2e130572680c9be3e717f61aef5915d
yukiees <yukiii8989@gmail.com>
2020-11-15 17:37:44 +0900

    oo4_comp

--- 004_passHack/ViewController.swift
+++ 004_passHack/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel4: UILabel!
     
     @IBOutlet weak var countLabel: UILabel!
+    var password: Int = 12
     
 
     override func viewDidLoad() {
@@ -23,9 +24,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func start(_ sender: Any) {
+        for i in 0...9999{
+            
+            countLabel.text = String(i)
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.0005))
+            
+            if i==password{
+                print("パスワードは\(i)です")
+                var digits = [Int]()
+                
+                for _ in 0...3{
+                    digits.append(password % 10)
+                    password = password / 10
+                }
+                resultLabel1.text = String(digits[0])
+                resultLabel2.text = String(digits[1])
+                resultLabel3.text = String(digits[2])
+                resultLabel4.text = String(digits[3])
+            }
+        }
     }
     
     @IBAction func reset(_ sender: Any) {
+        password = 1234
+        
+        countLabel.text = "startボタンで解析開始"
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
     }
     
 }
